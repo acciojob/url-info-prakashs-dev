74cbf46dfd18eb211e6dfc36d515eaffa3ad5278
Prakash S <79446554+prakashs-dev@users.noreply.github.com>
2023-11-15 12:56:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
 const currentUrl = window.location.href;
 const n = currentUrl.length;
-alert("The length of the URL is: ",n);
+alert("The length of the URL is: "+n);
